af20a438023f62792283c8e6bf65b57a7687a455
stathiw <stathi.weir@gmail.com>
2024-06-13 12:09:09 +1000

    Implement Code arithmetic class (with undesired behaviour)

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -7,5 +7,10 @@ The format is based on [Keep a Changelog](https://keepachangelog.com/en/1.0.0/)
 
 ## 2024-06-13
 ### Added
+- Add Code python implementation
+- (At the time the author forgot to handle non-float values, so the code is not working as expected)
+
+## 2024-06-13
+### Added
 - Add version.txt file
 
---
+++ code.py
@@ -0,0 +1,28 @@
+# Description: This is a simple python code that performs basic arithmetic operations.
+
+class Code:
+    def __init__(self):
+        value = 0
+
+    def add(self, x, y):
+        return x + y
+
+    def subtract(self, x, y):
+        return x - y
+
+    def multiply(self, x, y):
+        return x * y
+
+    def divide(self, x, y):
+        return x / y
+
+
+if __name__ == '__main__':
+    print("Hello World")
+
+    code = Code()
+    
+    print(code.add(1, 2))       # Output: 3
+    print(code.subtract(1, 2))  # Output: -1
+    print(code.multiply(1, 2))  # Output: 2
+    print(code.divide(1, 2))    # Output: 0.5 (the current implementation is not correct)
