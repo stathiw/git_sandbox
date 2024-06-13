d2dd23698e6c297c84bd51df0b76d1968ef54252
stathiw <stathi.weir@gmail.com>
2024-06-13 13:35:26 +1000

    Change name of Code class to Calculator

--- code.py
+++ code.py
@@ -1,6 +1,6 @@
 # Description: This is a simple python code that performs basic arithmetic operations.
 
-class Code:
+class Calculator:
     def __init__(self):
         value = 0
 
@@ -19,14 +19,17 @@ class Code:
     def exponent(self, x, y):
         return x ** y
 
+    def modulus(self, x, y):
+        return x % y
+
 
 if __name__ == '__main__':
     print("Hello World")
 
-    code = Code()
+    calc = Calculator()
     
-    print(code.add(1, 2))       # Output: 3
-    print(code.subtract(1, 2))  # Output: -1
-    print(code.multiply(1, 2))  # Output: 2
-    print(code.divide(1, 2))    # Output: 0.5 (the current implementation is not correct)
-    print(code.exponent(2, 3))  # Output: 8
+    print(calc.add(1, 2))       # Output: 3
+    print(calc.subtract(1, 2))  # Output: -1
+    print(calc.multiply(1, 2))  # Output: 2
+    print(calc.divide(1, 2))    # Output: 0.5 (the current implementation is not correct)
+    print(calc.exponent(2, 3))  # Output: 8
